d0534570ea7a38ae2c0ba17c6e58657ebbd34835
Amy Pierce <diablokitty@gmail.com>
2022-08-02 11:37:47 -0700

    update to pull

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 
+//USER INPUT FORM
+let form = document.getElementById('NEED HTML');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  clickAllowed = parseInt(event.target.NEEDHTML.value);
+  form.reset();
+}
+
+form.addEventListener('submit', handleSubmit);
+
+
+
+
+
+//FIGHT ACTION SELECTION FORM
+let form = document.getElementById('NEED HTML');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  clickAllowed = parseInt(event.target.NEEDHTML.value);
+  form.reset();
+}
+
+form.addEventListener('submit', handleSubmit);
