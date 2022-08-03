3f190fc502969b6834a8288e4d3fe00b0d2ee47e
Hanbyeol <davidlee9088@gmail.com>
2022-08-03 13:42:43 -0700

    render up waiting for function combined

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
+//DECLARE GLOBAL VARIABLES
+
+let player;
+// let chr1 = document.getElementById('avatar1')
+// let chr2 = document.getElementById('avatar2')
+// let chr3 = document.getElementById('avatar3')
+// let chr4 = document.getElementById('avatar4')
+let boss;
+let bossArr = [];
+
+
+
 
 function Render() {
 
-  let getBody = document.querySelector(".body")
-  getBody.innerHTML = '<section><div class="headerInner"><p id="test">HP: Enemy</p>' + '<img src="https://via.placeholder.com/300x375/ddd"></div></section><section><div class="headerInner"><p id="test">HP: Player</p>' + '<img src="https://via.placeholder.com/300x375/ddd"></div></section><button type="button">Ablilty1</button>' + '<button type="button">Ablilty2</button>' + '<button type="button">Ablilty3</button>' + '<button type="button">Ablilty4</button>';
+  let gamePlayScreen = document.querySelector(".gamePlayScreen")
+  gamePlayScreen.innerHTML = 'Section<div class="headerInner"><p id="test">HP: Enemy</p>' + '<img src="https://via.placeholder.com/300x375/ddd"></div></section><section><div class="headerInner"><p id="test">HP: Player</p>' + '<img src="https://thumbs.dreamstime.com/z/tough-man-18388218.jpg""></div></section><button type="button" onclick="attack()">Attack</button>' + '<button type="button">Defense</button>' + '<button type="button">S Attack</button>';
 
 
 }
@@ -14,15 +26,39 @@ function Render() {
 
 
 
-//FIGHT ACTION SELECTION FORM
-let form = document.getElementById('newUser');
 
+function Player(playerName, avatarChoice, attack, defense, specialAttack, highestLevelCompleted = 0, currentLevel = 0, totalWins = 0, totalLosses = 0) {
+  this.playerName = playerName;
+  this.avatar = avatarChoice;
+  this.attack = attack;
+  this.defense = defense;
+  this.specialAttack = specialAttack;
+  this.healthPoints = 100;
+  this.highestLevelCompleted = highestLevelCompleted;
+  this.currentLevel = currentLevel;
+  this.totalWins = totalWins;
+  this.totalLosses = totalLosses;
+ 
+}
+
+
+// //USER INPUT FORM
+let form = document.getElementById('newUser');
 function handleSubmit(event) {
+  
   event.preventDefault();
-  clickAllowed = parseInt(event.target.newUser.value);
+  let newUser = event.target.newUser.value;
+  let avatarChoice = event.target.chooseAvatar.value;
   form.reset();
+  console.log(avatarChoice);
+  return;
 }
 
 form.addEventListener('submit', handleSubmit);
 
+
+
+
+
+//DECLARE GLOBAL VARIABLES
 
