68cfc1eda6302245b6368729c6a2e82525b1c148
Amy Pierce <diablokitty@gmail.com>
2022-08-02 18:28:15 -0700

    buttons and functionality for reference

--- js/app.js
+++ js/app.js
@@ -1,99 +1,35 @@
 let player;
 let boss;
 
-function Player(userName,attack,defense,specialAttack,highestLevelCompleted,currentLevel,totalWins,totalLosses,fileExtension = 'jpg') 
-{
-  this.userName = userName;
-  this.attack = attack;
-  this.defense = defense;
-  this.specialAttack = specialAttack;
-  this.healthPoints = healthPoints;
-  this.highestLevelCompleted = highestLevelCompleted;
-  this.currentLevel = currentLevel;
-  this.totalWins = totalWins;
-  this.totalLosses = totalLosses;
-  this.src = `images/${this.playerType}.${fileExtension}`;
+//USER INPUT FORM
+let form = document.getElementById('newUser');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  newUserName = event.target.name.value;
+  avatarChoice = event.target.chooseAvatar.value;
+  form.reset();
 }
 
-function Boss(bossType,bAttack,bDefense,bSpecialAttack,bHealthPoints,fileExtension = 'jpg')
-{
-  this.bossType = bossType;
-  this.bAttack = bAttack;
-  this.bDefense = bDefense;
-  this.bSpecialAttack = bSpecialAttack;
-  this.bHealthPoints = bHealthPoints;
-  this.bSrc = `images/${this.bossType}.${fileExtension}`;
-}
+//form.addEventListener('submit', handleSubmit);
 
-let GameManger = {
-  setGameStart: function(playerType) {
-this.resetPlayer(playerType);
-this.setPreFight();
-  },
-  resetPlayer: function(PlayerType) {
-      switch (PlayerType) {
-          case "Player1":
-              player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-              break;
-          case "Player2":
-              player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-              break;
-          case "Player3":
-                player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-                break;
-          case "Player4":
-                  player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-                  break;
-              // playerType,attack,defense,specialAttack,highestLevelCompleted,currentLevel,totalWins,totalLosses,image
-      }
 
-  },
-  setPreFight: function() {
-      let getHeader = document.querySelector(".header");
-      let getActions = document.querySelector(".actions");
-      let getArena = document.querySelector(".arena");
-      getHeader.innerHTML = '<p>Task:Looking for a fight</p>'
-      getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManger.setFight()">Search for an enemy!</a>';
-      getArena.style.visibility = "visible";
-  }, 
-  setFight: function() {
-      let getHeader = document.querySelector(".header");
-      let getActions = document.querySelector(".actions");
-      let getDefense = document.querySelector(".defense")
-      let getSpecialMove = document.querySelector('.specialMove');
-      let getEnemy = document.querySelector(".enemy");
-      // Create enemey!
-      // Attack
-      // Defense
-      // Special Attack
-      // Health Points
-      // Image
-      let boss1 = new Boss(bossType, 100, 100, 100, 100, placeholder);
-      let boss2 = new Boss(bossType, 100, 100, 100, 100, placeholder);
-      let boss3 = new Boss(bossType, 100, 100, 100, 100, placeholder);
-      let boss4 = new Boss(bossType, 100, 100, 100, 100, placeholder);
+//connect move buttons code to the DOM
+let moveSection = document.querySelector('#moveSection');
+let attack = document.querySelector('#moveSection button:first-child');
+let defend = document.querySelector('#moveSection button:nth-child(2)');
+let specialMove = document.querySelector('#moveSection button:nth-child(3)');
 
 
 
+//FIGHT ACTION SELECTION FUNCTION
+function getMoveChoice(event) {
+console.log(event.target.id);
+  if (event.target.id === 'moveSection') {
 
-      switch (stageBossChoice) {
-          case 0:
-          boss = boss1;
-          break;
-          case 1:
-          boss = boss2;
-          break;
-          case 2:
-          boss = boss3;
-          break;
-          case 3:
-          boss = boss4;
-          break;
-      }
-      getHeader.innerHTML = '<p>Task: Choose your move</p>';
-      getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
-      getDefense.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcDefense()">Defense!</a>';
-      getSpecialMove.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcSpecialMove()">Special Move!</a>';
-      // getEnemy.innerHTML = '<img src="images/boss/' + boss.bossType.toLowerCase() + '.jpeg"' + '"class=img-avatar"><div><h3>' + '</h3><p>Health: ' + boss.health + '</p>'
-    }
+      alert('Move or be moved.');
   }
+  let moveChoice = event.target.id;
+  return moveChoice;
+}
+moveSection.addEventListener('click', getMoveChoice);
