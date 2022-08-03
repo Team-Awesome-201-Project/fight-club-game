a699e16ffc46d30655bb4eb85c99d1ea3ec239b6
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-08-03 09:05:35 -0700

    changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 let player;
 let boss;
 
-function Player(userName,attack,defense,specialAttack,highestLevelCompleted,currentLevel,totalWins,totalLosses,fileExtension = 'jpg') 
-{
+// ========== Player constructor and prototypes ==========
+
+function Player(userName, attack, defense, specialAttack, highestLevelCompleted, currentLevel, totalWins, totalLosses, fileExtension = 'jpg') {
   this.userName = userName;
   this.attack = attack;
   this.defense = defense;
@@ -15,85 +16,166 @@ function Player(userName,attack,defense,specialAttack,highestLevelCompleted,curr
   this.src = `images/${this.playerType}.${fileExtension}`;
 }
 
-function Boss(bossType,bAttack,bDefense,bSpecialAttack,bHealthPoints,fileExtension = 'jpg')
-{
+Player.prototype.newHealth = function (bossIncDmg) {
+  console.log(this.bHealthPoints);
+  this.bHealthPoints -= bossIncDmg;
+  console.log(this.bHealthPoints);
+}
+
+Player.prototype.attackStart = function (min, max) {
+  console.log('player chose to attck!');
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  let crit = criticalStart();
+  console.log(criticalStart());
+  if (crit === false) {
+      return playerIncDmg += Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  else if (crit === true) {
+      return playerIncDmg += Math.floor(Math.random() * (max - min + 1) + min) * 2;
+  }
+}
+
+Player.prototype.specialAttackStart = function (min, max) {
+  console.log('boss chose to attck!');
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  let crit = criticalStart();
+  if (crit === false) {
+      return bossIncDmg += Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  else if (crit === true) {
+      return bossIncDmg += Math.floor(Math.random() * (max - min + 1) + min) * 2;
+  }
+}
+
+Player.prototype.defendStart = function (){
+  return playerDefense = true;
+}
+
+// ========== Boss constructor and prototypes ==========
+
+function Boss(bossType, bAttack, bDefense, bSpecialAttack, bHealthPoints, fileExtension = 'jpg') {
   this.bossType = bossType;
   this.bAttack = bAttack;
   this.bDefense = bDefense;
   this.bSpecialAttack = bSpecialAttack;
   this.bHealthPoints = bHealthPoints;
   this.bSrc = `images/${this.bossType}.${fileExtension}`;
+
+
+
 }
 
-let GameManger = {
-  setGameStart: function(playerType) {
-this.resetPlayer(playerType);
-this.setPreFight();
-  },
-  resetPlayer: function(PlayerType) {
-      switch (PlayerType) {
-          case "Player1":
-              player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-              break;
-          case "Player2":
-              player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-              break;
-          case "Player3":
-                player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-                break;
-          case "Player4":
-                  player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
-                  break;
-              // playerType,attack,defense,specialAttack,highestLevelCompleted,currentLevel,totalWins,totalLosses,image
-      }
+Boss.prototype.newHealth = function (playerIncDmg) {
+  console.log(this.bHealthPoints);
+  this.bHealthPoints -= playerIncDmg;
+  console.log(this.bHealthPoints);
+}
 
-  },
-  setPreFight: function() {
-      let getHeader = document.querySelector(".header");
-      let getActions = document.querySelector(".actions");
-      let getArena = document.querySelector(".arena");
-      getHeader.innerHTML = '<p>Task:Looking for a fight</p>'
-      getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManger.setFight()">Search for an enemy!</a>';
-      getArena.style.visibility = "visible";
-  }, 
-  setFight: function() {
-      let getHeader = document.querySelector(".header");
-      let getActions = document.querySelector(".actions");
-      let getDefense = document.querySelector(".defense")
-      let getSpecialMove = document.querySelector('.specialMove');
-      let getEnemy = document.querySelector(".enemy");
-      // Create enemey!
-      // Attack
-      // Defense
-      // Special Attack
-      // Health Points
-      // Image
-      let boss1 = new Boss(bossType, 100, 100, 100, 100, placeholder);
-      let boss2 = new Boss(bossType, 100, 100, 100, 100, placeholder);
-      let boss3 = new Boss(bossType, 100, 100, 100, 100, placeholder);
-      let boss4 = new Boss(bossType, 100, 100, 100, 100, placeholder);
-
-
-
-
-      switch (stageBossChoice) {
-          case 0:
-          boss = boss1;
-          break;
-          case 1:
-          boss = boss2;
-          break;
-          case 2:
-          boss = boss3;
-          break;
-          case 3:
-          boss = boss4;
-          break;
-      }
-      getHeader.innerHTML = '<p>Task: Choose your move</p>';
-      getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
-      getDefense.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcDefense()">Defense!</a>';
-      getSpecialMove.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcSpecialMove()">Special Move!</a>';
-      // getEnemy.innerHTML = '<img src="images/boss/' + boss.bossType.toLowerCase() + '.jpeg"' + '"class=img-avatar"><div><h3>' + '</h3><p>Health: ' + boss.health + '</p>'
-    }
+Boss.prototype.attackStart = function (min, max) {
+  console.log('boss chose to attck!');
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  let crit = criticalStart();
+  if (crit === false) {
+      return bossIncDmg += Math.floor(Math.random() * (max - min + 1) + min);
   }
+  else if (crit === true) {
+      return bossIncDmg += Math.floor(Math.random() * (max - min + 1) + min) * 2;
+  }
+}
+
+Boss.prototype.specialAttackStart = function (min, max) {
+  console.log('boss chose to attck!');
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  let crit = criticalStart();
+  if (crit === false) {
+      return bossIncDmg += Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  else if (crit === true) {
+      return bossIncDmg += Math.floor(Math.random() * (max - min + 1) + min) * 2;
+  }
+}
+
+// Boss.prototype.defendStart = function (){
+//   return bossDefense = true;
+// }
+
+// GameManager, runs
+
+let GameManger = {
+  setGameStart: function (playerType) {
+    this.resetPlayer(playerType);
+    this.setPreFight();
+  },
+  resetPlayer: function (PlayerType) {
+    switch (PlayerType) {
+      case "Player1":
+        player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
+        break;
+      case "Player2":
+        player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
+        break;
+      case "Player3":
+        player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
+        break;
+      case "Player4":
+        player = new Player(playerType, 100, 100, 100, 0, 0, 0, 0, placeholder);
+        break;
+      // playerType,attack,defense,specialAttack,highestLevelCompleted,currentLevel,totalWins,totalLosses,image
+    }
+
+  },
+  setPreFight: function () {
+    let getHeader = document.querySelector(".header");
+    let getActions = document.querySelector(".actions");
+    let getArena = document.querySelector(".arena");
+    getHeader.innerHTML = '<p>Task:Looking for a fight</p>'
+    getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManger.setFight()">Search for an enemy!</a>';
+    getArena.style.visibility = "visible";
+  },
+  setFight: function () {
+    let getHeader = document.querySelector(".header");
+    let getActions = document.querySelector(".actions");
+    let getDefense = document.querySelector(".defense")
+    let getSpecialMove = document.querySelector('.specialMove');
+    let getEnemy = document.querySelector(".enemy");
+    // Create enemey!
+    // Attack
+    // Defense
+    // Special Attack
+    // Health Points
+    // Image
+    let boss1 = new Boss(bossType, 100, 100, 100, 100, placeholder);
+    let boss2 = new Boss(bossType, 100, 100, 100, 100, placeholder);
+    let boss3 = new Boss(bossType, 100, 100, 100, 100, placeholder);
+    let boss4 = new Boss(bossType, 100, 100, 100, 100, placeholder);
+
+
+
+    switch (stageBossChoice) {
+      case 0:
+        boss = boss1;
+        break;
+      case 1:
+        boss = boss2;
+        break;
+      case 2:
+        boss = boss3;
+        break;
+      case 3:
+        boss = boss4;
+        break;
+    }
+    getHeader.innerHTML = '<p>Task: Choose your move</p>';
+    getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
+    getDefense.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcDefense()">Defense!</a>';
+    getSpecialMove.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcSpecialMove()">Special Move!</a>';
+    // getEnemy.innerHTML = '<img src="images/boss/' + boss.bossType.toLowerCase() + '.jpeg"' + '"class=img-avatar"><div><h3>' + '</h3><p>Health: ' + boss.health + '</p>'
+  }
+}
+
+let boss1 = new Boss('Player', [5, 10], 10, [15, 10], 100, 'test');
+let boss2 = new Boss('Boss', [15, 20], 10, [20, 30], 100, 'test');
--- js/fightsequence.js
+++ js/fightsequence.js
@@ -1,58 +1,6 @@
 'use strict';
 
-let attack = [5, 10];
-let defense = 5;
-let specialAttack = [15, 20];
-let healthPoints = 100;
 
-let bAttack = [5, 10];
-let bDefense = 5;
-let bSpecialAttack = [15, 20];
-let bHealthPoints = 100;
-
-
-// console.log(healthPoints);
-// console.log(test());
-
-// Attack function (takes both "attack" and "special attack" moves)
-// Use <button onclick ="attack(atk[0], atk[1])">Attack</button> to call for attack
-// Use <button onclick ="attack(spa[0], spa[1])">Attack</button> to call for special attack
-function attackStart(min, max, hp) {
-    min = Math.ceil(min);
-    max = Math.ceil(max);
-    hp = Math.ceil(hp);
-    let crit = critical();
-    if (crit === false) {
-        let dmg = Math.floor(Math.random() * (max - min + 1) + min);
-        return hp = hp - dmg;
-    }
-    else if (crit === true) {
-        let critDmg = Math.floor(Math.random() * (max - min + 1) + min) * 2;
-        return hp = hp - critDmg;
-    }
-}
-
-
-// Call to reduce HP 
-function newBHp() {
-    return bHealthPoints = attack(atk[0], atk[1], bHealthPoints);
-}
-console.log(newBHp());
-
-// Call to reduce HP 
-function newHp() {
-    return healthPoints = attack(bAttack[0], bAttack[1], healthPoints);
-}
-console.log(newHp());
-
-// Defend function
-// Use <button onclick ="defend()">Attack</button> to call
-function defendStart(num1, num2) {
-    num1 = Math.ceil(num1);
-    return Math.floor(num1 - num2);
-}
-
-// Critical strike function
 function criticalStart() {
     let crit = Math.floor(Math.random() * 100);
     if (crit <= 10) {
@@ -61,35 +9,27 @@ function criticalStart() {
     return false;
 }
 
-// Healh reduction
-function minusHealth() {
+function gameLogic(playerButtonClick) {
+    let playerTurn = '';
+    let bossTurn = '';
+    let playerIncDmg = 0;
+    let bossIncDmg = 0;
+    let playerDefense = false;
+    // let bossDefense = false;
 
-}
-
-// Tests the HP reduction 
-function test() {
-    while (healthPoints > 0) {
-        console.log(attack(atk[0], atk[1]));
+    if (playerButtonClick = 'attack') {
+        player1.attackStart(player1.attack[0], attack[1]);
     }
-    console.log('Game Over!');
-    return healthPoints = 0;
-}
 
-// function newHealthValue(oldValue) {
-//     oldValue = 
-//     return hp; 
-// }
-// console.log(newHealthValue);
+    else if (playerButtonClick = 'special attack') {
+        player1.specialAttackStart(player1.specialAttack[0], specialAttack[1]);
+    }
 
-function playerSelectAttack() {
-    attackStart(attack[0], attack[1], bHealthPoints); 
-}
+    else if (playerButtonClick = 'defend') {
+        player.defendStart();
+    }
 
-function playerSelectSpecialAttack() {
-    attackStart(specialAttack[0], specialAttack[1], bHealthPoints); 
-}
 
-function playerSelectDefense() {
-    defendStart
+
 
 }
