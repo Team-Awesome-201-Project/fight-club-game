a085d6932bb6efe3794dc1495a10149a5b28b212
Matteus J Guerra <matteusguerra@gmail.com>
2022-08-03 14:46:13 -0700

    commit

--- js/app.js
+++ js/app.js
@@ -171,20 +171,20 @@ let bossArray = [boss1];
 console.log(playerArray[0]);
 console.log(bossArray[0]);
 
-function Render() {
+  function Render() {
 
-  let getHeader = document.querySelector(".header");
-  let getLeftside = document.querySelector(".leftSide");
-  let getBody = document.querySelector('.body');
-  let getFooter = document.querySelector('.footer');
-  let getMain = document.querySelector('.main')
-  getHeader.innerHTML = '<div class="headerInner"><p id="test">HP: Enemy</p>' + '<img src="https://via.placeholder.com/300x375/ddd"></div>';
-  getLeftside.innerHTML = '<p>HP: User</p>' + '<img src="https://via.placeholder.com/300x375/ddd">';
-  getBody.innerHTML = '<button type="button">Ablilty1</button>' + '<button type="button">Ablilty2</button>' + '<button type="button">Ablilty3</button>' + '<button type="button">Ablilty4</button>'
-  getFooter.innerHTML = '<p>Good luck</p>'
-  getMain.innerHTML = '<p class="test3">Test2</p>'
-
-
+    let getHeader = document.querySelector(".header");
+    let getLeftside = document.querySelector(".leftSide");
+    let getBody = document.querySelector('.body');
+    let getFooter = document.querySelector('.footer');
+    let getMain = document.querySelector('.main')
+    getHeader.innerHTML = '<div class="headerInner"><p id="test">HP: Enemy</p>' + '<img src="https://via.placeholder.com/300x375/ddd"></div>';
+    getLeftside.innerHTML = '<p>HP: User</p>' + '<img src="https://via.placeholder.com/300x375/ddd">';
+    getBody.innerHTML = '<button type="button">Ablilty1</button>' + '<button type="button">Ablilty2</button>' + '<button type="button">Ablilty3</button>' + '<button type="button">Ablilty4</button>'
+    getFooter.innerHTML = '<p>Good luck</p>'
+    getMain.innerHTML = '<p class="test3">Test2</p>'
 
 
+
+  }
 }
